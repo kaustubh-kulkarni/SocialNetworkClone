8f7b9281dbad78f7c030e60983e73c812b480950
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-04-08 14:13:43 +0200

    Added a toolbar for header

--- SocialNetworkClone/src/app/app.module.ts
+++ SocialNetworkClone/src/app/app.module.ts
@@ -5,22 +5,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatInputModule } from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 // Components
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './components/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatToolbarModule,
     FormsModule,
     BrowserAnimationsModule
   ],
