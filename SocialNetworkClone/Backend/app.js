123df061274085b584a3fdbaad93b77601a2dc59
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-04-25 17:44:42 +0200

    Updated app.js with CORS support

--- SocialNetworkClone/Backend/app.js
+++ SocialNetworkClone/Backend/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 
 const app = express();
-// Middleware function
+
+// CORS enable header
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+    next();
+});
+// Get posts endpoint
 app.use('/api/posts', (req, res, next) => {
     const posts = [
         { id: '1', title: 'First server side post', content: 'Coming from server' },
