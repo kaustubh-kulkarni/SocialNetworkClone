4010490a54fb7493f61af7727ad21ece7f0ae40f
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-04-25 17:27:27 +0200

    Updated express code with some seed post data and path

--- SocialNetworkClone/Backend/app.js
+++ SocialNetworkClone/Backend/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const app = express();
 // Middleware function
-app.use((req, res, next) => {
-    console.log("first middleware");
-    next();
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        { id: '1', title: 'First server side post', content: 'Coming from server' },
+        { id: '2', title: 'Second server side post', content: 'Coming from server' }
+    ];
+    res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: posts
+    })
 });
 
-app.use((req, res, next) => {
-    res.send("Hello from express");
-});
+
 
 module.exports = app;
