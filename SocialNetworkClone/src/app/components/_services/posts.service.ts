8093d404e5b24eea81b0747a357a3ba8ffab8561
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-04-26 14:03:47 +0200

    Updated delete method in post service angular to delete the post from frontend as well

--- SocialNetworkClone/src/app/components/_services/posts.service.ts
+++ SocialNetworkClone/src/app/components/_services/posts.service.ts
@@ -42,7 +42,9 @@ export class PostsService {
 
     deletePost(postId: string){
         this.http.delete(this.baseUrl + '/posts/' + postId).subscribe(() => {
-            console.log('Deleted');
+            const updatedPosts = this.posts.filter(post => post.id !== postId);
+            this.posts = updatedPosts;
+            this.postsUpdated.next([...this.posts]);
         });
     }
 }
