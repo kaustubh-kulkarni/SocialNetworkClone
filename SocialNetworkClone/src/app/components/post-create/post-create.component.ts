df4a84362f79ce9f4cf67123203da004fb85bca0
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2021-04-27 17:01:43 +0200

    Added file picking functionality in app

--- SocialNetworkClone/src/app/components/post-create/post-create.component.ts
+++ SocialNetworkClone/src/app/components/post-create/post-create.component.ts
@@ -26,7 +26,8 @@ export class PostCreateComponent implements OnInit {
   ngOnInit(): void {
     this.form = new FormGroup({
       'title': new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
-      'content': new FormControl(null, {validators: [Validators.required]})
+      'content': new FormControl(null, {validators: [Validators.required]}),
+      'image': new FormControl(null, {validators: [Validators.required]})
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -44,6 +45,16 @@ export class PostCreateComponent implements OnInit {
       }
     });
   }
+
+  onImagePicked(event: Event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    console.log(this.form);
+  }
+
+
   // To create a post
   onSavePost() {
     if(this.form.invalid){
